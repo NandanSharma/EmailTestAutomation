cc6812e712c6667d89cda3cbe2b67597e5e54f83
NandanSharma <20073218+NandanSharma@users.noreply.github.com>
2024-12-27 20:20:05 +0530

    updating few items

--- src/main/java/com/email/qa/ExtentReportListener/ExtentReporterNG.java
+++ src/main/java/com/email/qa/ExtentReportListener/ExtentReporterNG.java
@@ -14,62 +14,60 @@ import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.xml.XmlSuite;
 
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 public class ExtentReporterNG implements IReporter {
-	private ExtentReports extent;
+    private ExtentReports extent;
 
-	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites,
-			String outputDirectory) {
-		extent = new ExtentReports(outputDirectory + File.separator
-				+ "email_Test_automation_Extent.html", true);
+    public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
+        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(
+				outputDirectory + File.separator + "email_Test_automation_Extent.html");
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
 
-		for (ISuite suite : suites) {
-			Map<String, ISuiteResult> result = suite.getResults();
+        for (ISuite suite : suites) {
+            Map<String, ISuiteResult> result = suite.getResults();
 
-			for (ISuiteResult r : result.values()) {
-				ITestContext context = r.getTestContext();
+            for (ISuiteResult r : result.values()) {
+                ITestContext context = r.getTestContext();
 
-				buildTestNodes(context.getPassedTests(), LogStatus.PASS);
-				buildTestNodes(context.getFailedTests(), LogStatus.FAIL);
-				buildTestNodes(context.getSkippedTests(), LogStatus.SKIP);
-			}
-		}
+                buildTestNodes(context.getPassedTests(), Status.PASS);
+                buildTestNodes(context.getFailedTests(), Status.FAIL);
+                buildTestNodes(context.getSkippedTests(), Status.SKIP);
+            }
+        }
 
-		extent.flush();
-		extent.close();
-	}
+        extent.flush();
+    }
 
-	private void buildTestNodes(IResultMap tests, LogStatus status) {
-		ExtentTest test;
+    private void buildTestNodes(IResultMap tests, Status status) {
+        ExtentTest test;
 
-		if (tests.size() > 0) {
-			for (ITestResult result : tests.getAllResults()) {
-				test = extent.startTest(result.getMethod().getMethodName());
+        if (tests.size() > 0) {
+            for (ITestResult result : tests.getAllResults()) {
+                test = extent.createTest(result.getMethod().getMethodName());
 
-				test.setStartedTime(getTime(result.getStartMillis()));
-				test.setEndedTime(getTime(result.getEndMillis()));
+                test.getModel().setStartTime(getTime(result.getStartMillis()));
+                test.getModel().setEndTime(getTime(result.getEndMillis()));
 
-				for (String group : result.getMethod().getGroups())
-					test.assignCategory(group);
+                for (String group : result.getMethod().getGroups())
+                    test.assignCategory(group);
 
-				if (result.getThrowable() != null) {
-					test.log(status, result.getThrowable());
-				} else {
-					test.log(status, "Test " + status.toString().toLowerCase()
-							+ "ed");
-				}
+                if (result.getThrowable() != null) {
+                    test.log(status, result.getThrowable());
+                } else {
+                    test.log(status, "Test " + status.toString().toLowerCase() + "ed");
+                }
+            }
+        }
+    }
 
-				extent.endTest(test);
-			}
-		}
-	}
-
-	private Date getTime(long millis) {
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTimeInMillis(millis);
-		return calendar.getTime();
-	}
+    private Date getTime(long millis) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(millis);
+        return calendar.getTime();
+    }
 }
